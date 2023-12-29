119bc4314de9bb09a95bc54f73f2b95ad5178cb4
Aman Pandey <amanp.jun1906@gmail.com>
2023-12-29 14:27:35 +0530

    front page

---
+++ src/Component/NewsFront/Newsfront.jsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react'
+import '../NewsFront/newsFront.css'
+const Newsfront = () => {
+
+    const[newsHead,setNewsHead]=useState([])
+  
+    const newsData =async()=>{
+        
+      // const url1 = `https://newsapi.org/v2/everything?q=${news}&from=2023-11-28&sortBy=publishedAt&apiKey=5337793ea772470eb9685268daa6b81e`;  
+      // const url = `https://newsapi.org/v2/everything?q=india&from=2023-11-28&sortBy=publishedAt&apiKey=8c147bd042ad4d3a9aaa56bf1135db4d`;
+       const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=5bb8df76f578431f80c85a4c9bd311ac`;
+       
+       
+        const response = await fetch(url);
+        const data = await response.json();
+        
+        console.log(data.articles);
+        setNewsHead(data.articles);
+       
+    };
+    
+    useEffect(()=>{
+      newsData();
+    },[])
+      
+
+  return (
+    <div>
+    {/* <div className="webFront">
+    <div className="logo">
+        <p>News<span>.Web</span></p>
+    </div>
+      <div className="webData">
+    <div className="article">
+          <img src="https://img.etimg.com/thumb/msid-83515267,width-1200,height-630,imgsize-853209,overlay-etmarkets/photo.jpg" alt="" />
+          <div className="titleDes">
+          <h2>Stock market update: Nifty Bank index advances 1.17%</h2>
+          <p className='desc'>The Nifty Bank index closed 1.17 per cent  up  at 48282.2.</p>
+          </div>
+          <hr />
+          <div className="authDate">
+            <p className='author'>ETMarkets.com</p>
+          </div>
+      </div>
+      </div>
+    </div> */}
+    
+      <div className="webFront">
+      <div className="logo">
+        <p>News<span>.Web</span></p>
+      </div>
+      <h2>Top Headlines</h2>
+      <div className="webData">
+      {newsHead?.map((article,index)=>(
+          <div key={index} className="article">
+            <img src={article?.urlToImage} alt="" />
+            <div className="titleDes">
+              <h4>{article?.title}</h4>
+            </div>
+              <hr />
+            <div className="authDate">
+              <p className='author'>{article?.author}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      
+      </div>
+      <hr className='end'/>
+    </div>
+    
+  )
+}
+
+export default Newsfront
