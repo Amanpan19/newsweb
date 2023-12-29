5d7b398ffbb23eead0d5308e647b31eb6f70a558
Aman Pandey <amanp.jun1906@gmail.com>
2023-12-29 14:29:13 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Component/NavBar/Navbar';
+import Categories from './Pages/Categories';
+import Home from './Pages/Home';
+import Footer from './Component/Footer/Footer';
+import News from './Component/News/News';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/news' element={<News/>}/>
+          <Route path='/health' element={<Categories type="health"/>}/>
+          <Route path='/sports' element={<Categories type="sports"/>}/>
+          <Route path='/tech' element={<Categories type="technology"/>}/>
+          <Route path='/science' element={<Categories type="science"/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
