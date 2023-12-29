db7c43fa215fee805485d84b33435e5cb74fd699
Aman Pandey <amanp.jun1906@gmail.com>
2023-12-29 14:54:05 +0530

    print in console

--- src/Pages/Categories.js
+++ src/Pages/Categories.js
@@ -2,6 +2,7 @@ import React from 'react'
 import News from '../Component/News/News'
 
 const Categories = (props) => {
+  console.log(props.key);
   return (
     <div>
       <News type={props.type}/>
